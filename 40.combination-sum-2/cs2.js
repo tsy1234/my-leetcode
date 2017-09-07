5a6a7e3d961c2c86cc61176a490b8ac37ef2be01
tsy1234 <siyu0852@icloud.com>
2017-09-07 21:41:06 +0800

    第40题js 好像是好了点

--- 40.combination-sum-2/cs2.js
+++ 40.combination-sum-2/cs2.js
@@ -3,7 +3,7 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum = function(candidates, target) {
+var combinationSum2 = function(candidates, target) {
     var n = candidates.length
     candidates.sort(function (left, right) {return left - right})
     var result = []
@@ -20,12 +20,13 @@ var backTracking = function (candidates, target, result, combination, begin) {
     }
 
     for (var i = begin;i < candidates.length && target >= candidates[i];i++) {
-        console.log(i)
         combination.push(candidates[i])
-        backTracking(candidates, target - candidates[i], result, combination, i)
-        combination.pop()
+        backTracking(candidates, target - candidates[i], result, combination, i + 1)
+        var pop = combination.pop()
+        while (i < candidates.length)
+            if (candidates[i + 1] === pop) i++
+            else break
     }
 }
 
-
-console.log(combinationSum([2, 3, 6, 7], 7))
+console.log(combinationSum2([1, 1, 2, 5, 6, 7, 10], 8))
