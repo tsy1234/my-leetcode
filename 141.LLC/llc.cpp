a243df2aa387873c914924cbfd1798d7ed0a3f4a
tsy1234 <siyu0852@icloud.com>
2017-12-11 20:49:58 +0800

    walker and runner

---
+++ 141.LLC/llc.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == NULL) return false;
+        ListNode* walker = head;
+        ListNode* runner = head;
+        while (runner.next && runner.next.next) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if (walker == runner) return true;
+        }
+        return false;
+    }
+};
