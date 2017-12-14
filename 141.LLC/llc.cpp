9eecadbc6d8fa5a0d89ab39466381f86dfd9201e
tsy1234 <siyu0852@icloud.com>
2017-12-14 09:18:20 +0800

    普通

--- 141.LLC/llc.cpp
+++ 141.LLC/llc.cpp
@@ -8,13 +8,17 @@
  */
  class Solution {
 public:
+    /*
+    使用两个指针walker和runner walker每次走一步
+    runner每次走两步 如果成环 那么在某一次一定能碰到一起
+    */
     bool hasCycle(ListNode *head) {
         if (head == NULL) return false;
         ListNode* walker = head;
         ListNode* runner = head;
-        while (runner.next && runner.next.next) {
-            walker = walker.next;
-            runner = runner.next.next;
+        while (runner->next && runner->next->next) {
+            walker = walker->next;
+            runner = runner->next->next;
             if (walker == runner) return true;
         }
         return false;
