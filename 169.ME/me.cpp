bf6904837ca29218d8376290a7d004bfc627995a
tsy1234 <siyu0852@icloud.com>
2017-12-18 17:46:44 +0800

    once

--- 169.ME/me.cpp
+++ 169.ME/me.cpp
@@ -1,6 +1,11 @@
 // Given an array of size n, find the majority element. 
 // The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 // You may assume that the array is non-empty and the majority element always exist in the array.
+
+/*
+ 根据题意 majority element 占总元素的一半以上
+ 则将数组排序后 数组中间的元素必然是me
+*/
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
