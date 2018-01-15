32c6dd0f8f462cee8de269c129d6fe1ca746712a
tsy1234 <siyu0852@icloud.com>
2018-01-15 10:37:31 +0800

    烦

--- 290/290.cpp
+++ 290/290.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string str) {
-        
+        int pa[26] {};
+        unordered_map<string, int> wr;
+        istringstream in(str);
+        int i = 0, n = pattern.size();
+        for (string word;in >> word;i++) {
+            if (i == n || pa[pattern[i] - 'a'] != wr[word])
+                return false;
+            pa[pattern[i] - 'a'] = wr[word] = i + 1; 
+        }
+        return i == n;
     }
 };
