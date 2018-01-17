f18851fdcbb0865bab0192b010ad0550f8f2aacc
tsy1234 <siyu0852@icloud.com>
2018-01-17 18:45:14 +0800

    O(1) space

--- 326/326.cpp
+++ 326/326.cpp
@@ -2,6 +2,6 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         // 1162261467 is 3^19,  3^20 is bigger than int  
-        return (n > 0 && 1162261467 % 3 == 0);
+        return (n > 0 && 1162261467 % n == 0);
     }
 };
