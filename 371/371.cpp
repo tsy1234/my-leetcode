3c61bd95d822ac511aca95e41acf123c8baebb50
tsy1234 <siyu0852@icloud.com>
2018-01-18 21:09:49 +0800

    位运算真难

--- 371/371.cpp
+++ 371/371.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
+    // 用与运算并左移一位得到进位，用异或运算得到不带进位的和，当进位为0,运算结束
     int getSum(int a, int b) {
-        
+        // 进位
+        int carry = 0;
+        while (b != 0) {
+            carry = a & b;
+            // 不带进位的和赋给a
+            a = a ^ b;
+            // 得到的进位赋予b
+            b = carry << 1;
+        }
+        return a;
+    }
+
+    // recursive
+    int recur(int a, int b) {
+        return b == 0 ? a : getSum(a ^ b, (a & b) << 1);
     }
 };
